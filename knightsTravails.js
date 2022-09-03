140d49ccd198f0c7d78b483d061abe4436fca67a
gefgu <gefgu@hotmail.com>
2022-09-03 09:11:54 -0300

    Add knightMoves algorithm pseudocode

--- knightsTravails.js
+++ knightsTravails.js
@@ -5,6 +5,20 @@
 // eg. knightMoves([0,0],[1,2]) == [[0,0],[1,2]]
 // knightMoves([0,0],[3,3]) == [[0,0],[1,2],[3,3]]
 
+// pseudocode
+// build a graph using all possible moves
+// from every possible move, keep going until destination is found.
+
+// The algorithm:
+// Compute all possible moves from starting position
+// For each of these, compute all possible moves.
+// Repeat until destination is found.
+// With all these data stored in a graph
+// search for the shortest path.
+
+// To do this, use knight which has a gameboard
+// Return the shortest path.
+
 
 // Gameboard
 // It shows if a square is a valid one.
