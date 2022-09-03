e83a859201cf1374ba2935c011dd35d078cedb51
gefgu <gefgu@hotmail.com>
2022-09-03 09:02:56 -0300

    Add game board and knight pseudocode overview

--- knightsTravails.js
+++ knightsTravails.js
@@ -4,3 +4,11 @@
 // knight will stop on along the way.
 // eg. knightMoves([0,0],[1,2]) == [[0,0],[1,2]]
 // knightMoves([0,0],[3,3]) == [[0,0],[1,2],[3,3]]
+
+
+// Gameboard
+// It shows if a square is a valid one.
+
+// Knight
+// It returns all possible moves (requires gameboard for validation)
+// from a position in the gameboard
