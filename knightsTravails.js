20c97354be96021b9cbc3b13f9576cf2c89b0063
gefgu <gefgu@hotmail.com>
2022-09-03 09:00:00 -0300

    Add knigthMoves pseudocode overview

---
+++ knightsTravails.js
@@ -0,0 +1,6 @@
+// knightMoves
+// shows the shortest possible way to get from one square 
+// to another by outputting all squares the 
+// knight will stop on along the way.
+// eg. knightMoves([0,0],[1,2]) == [[0,0],[1,2]]
+// knightMoves([0,0],[3,3]) == [[0,0],[1,2],[3,3]]
